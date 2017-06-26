357dd1f6198878e4ace0b2de02f31368b1e73cdb
tom <tom@localhost.localdomain>
2017-06-26 18:36:40 +0200

    new

---
+++ install_ethereum_environment.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+### for testrpc we need nodejs > 6.9.1
+#sudo apt-get install build-essential libssl-dev
+sudo apt-get install build-essential
+wget -qO- https://deb.nodesource.com/setup_8.x | sudo -E bash - 
+
+exit -1
+
+
+### we need golang in the exact version 1.8.1
+sudo curl -O https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
+sudo tar -xvf go1.8.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo mv go /usr/local
+sudo rm go1.8.1.linux-amd64.tar.gz
+export GOROOT=/usr/local/go
+export GOPATH=$PWD
+echo "verifying golang ..."
+echo
+echo
+$GOROOT/bin/go version
+echo
+echo
+echo "end go verification"
+### end golang
+
+### gets geth in version 1.6.6
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
+
+echo "verifying geth ..."
+echo
+echo
+geth version
+echo
+echo 
+echo "end geth verification"
+
+### install solicity compiler
+sudo apt-get install solc
+which solc
+
