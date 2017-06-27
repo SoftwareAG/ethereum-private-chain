bb159100fcad023d4a63117c16828c1a69082566
ThomasToepperwien <thomas.toepperwien@softwareag.com>
2017-06-27 10:36:27 +0200

    updated

--- install_ethereum_environment.sh
+++ install_ethereum_environment.sh
@@ -15,15 +15,15 @@ echo "*****************************************"
 
 ### for testrpc we need nodejs > 6.9.1
 cd ~
-sudo apt-get install build-essential
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+wget -qO- https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs
 npm install ethereumjs-testrpc
 sudo ln -s ~/node_modules/ethereumjs-testrpc/bin/testrpc /usr/bin/testrpc
-cd $currentDir
+#cd $currentDir
 
 ### we need golang in the exact version 1.8.1
-sudo curl -O https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
 sudo tar -xvf go1.8.1.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo mv go /usr/local
@@ -40,6 +40,7 @@ echo "end go verification"
 ### end golang
 
 ### gets geth in version 1.6.6
+cd $currentDir
 sudo apt-get install software-properties-common
 sudo apt-get update
 sudo apt-get install ethereum
