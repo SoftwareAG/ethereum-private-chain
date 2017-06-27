d9ace9b5de95ab7078afd158a2d33079fed3dd50
ThomasToepperwien <thomas.toepperwien@softwareag.com>
2017-06-27 10:00:01 +0200

    reviewed

--- install_solicity.sh
+++ install_solicity.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 currentPWD=$PWD
 cd ~
-#sudo apt install build-essential
-#sudo apt install cmake
 git clone --recursive https://github.com/ethereum/solidity.git
 cd solidity
 git submodule update --init --recursive
 ./scripts/install_deps.sh
+sudo ./scripts/build.sh install
 cd $currentPWD
