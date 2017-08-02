806c3686719b637532319fbe8161298c8098d209
tom <tom@localhost.localdomain>
2017-08-02 16:01:04 +0200

    introduced RPCADDR for binding

--- run_geth_console.sh
+++ run_geth_console.sh
@@ -2,6 +2,7 @@
 
 IDENTITY=Node1
 PORT=30301
+RPCADDR=192.168.122.231		#127.0.0.1
 RPCPORT=8101
 NETWORKID=4711
 DATADIR=./datadirs
@@ -18,7 +19,7 @@ if [ ! -d "$LOGDIR" ]; then
    mkdir "$LOGDIR"
 fi
 
-option_jungle="--password $PASSWORDFILE --mine --minerthreads=4 --fast --unlock $PRIMARY --identity $IDENTITY --datadir=$DATADIR/$IDENTITY -verbosity $VERBOSITY --port $PORT --rpc --rpcport $RPCPORT --rpccorsdomain \"*\" --networkid $NETWORKID --nodiscover --rpcapi \"db,eth,net,web3\""
+option_jungle="--password $PASSWORDFILE --mine --minerthreads=4 --fast --unlock $PRIMARY --identity $IDENTITY --datadir=$DATADIR/$IDENTITY -verbosity $VERBOSITY --port $PORT --rpc --rpcaddr $RPCADDR --rpcport $RPCPORT --rpccorsdomain \"*\" --networkid $NETWORKID --nodiscover --rpcapi \"db,eth,net,web3\""
 
 acc=(`geth $option_jungle account list | cut -d'{' -f2 | cut -d'}' -f1`)
 
