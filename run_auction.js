c8ee09b1a197f8e7baaeb4f3164c649f3fbd6ebf
tom <tom@localhost.localdomain>
2017-09-15 18:15:56 +0200

    run commands one by one

--- run_auction.js
+++ run_auction.js
@@ -1,3 +1,30 @@
+Copy the commands one by one !!!
+
+
+eth.defaultAccount=eth.accounts[0];
 eth.sendTransaction({"from":eth.accounts[0],"to":eth.accounts[1],"value":1000});
-eth.sendTransaction({"from":eth.accounts[0],"to":eth.accounts[2],"value":1000});
+eth.sendTransaction({"from":eth.accounts[0],"to":eth.accounts[2],"value":1001});
+console.log("account1 balance: "+eth.getBalance(eth.accounts[1]));
+console.log("account2 balance: "+eth.getBalance(eth.accounts[2]));
+personal.unlockAccount(eth.accounts[1]);
+personal.unlockAccount(eth.accounts[2]);
+console.log("auction bid from 1: "+auction.bid({"from":eth.accounts[1],"value":100}));
+console.log("account1 balance: "+eth.getBalance(eth.accounts[1]));
+console.log("account1 balance: "+eth.getBalance(eth.accounts[2]));
+var highest_bid = auction.getHighestBid.call();
+var highest_bidder = auction.getHighestBidder.call();
+console.log("highest bid: "+highest_bid);
+console.log("highest bidder: "+highest_bidder);
+console.log("auction bid from 2: "+auction.bid({"from":eth.accounts[2],"value":200}));
+console.log("account1 balance: "+eth.getBalance(eth.accounts[1]));
+console.log("account2 balance: "+eth.getBalance(eth.accounts[2]));
+var highest_bid = auction.getHighestBid.call();
+var highest_bidder = auction.getHighestBidder.call();
+console.log("highest bid: "+highest_bid);
+console.log("highest bidder: "+highest_bidder);
+console.log("account1 balance: "+eth.getBalance(eth.accounts[1]));
+console.log("account2 balance: "+eth.getBalance(eth.accounts[2]));
+console.log("withdrawRefund from 1: "+auction.withdrawRefund(eth.accounts[1]));
+console.log("account1 balance: "+eth.getBalance(eth.accounts[1]));
+console.log("account2 balance: "+eth.getBalance(eth.accounts[2]));
 
